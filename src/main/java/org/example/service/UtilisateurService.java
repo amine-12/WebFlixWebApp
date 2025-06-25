c1fdb59dc28449da78fe553309ef0df6c7f0a3b8
MaxenceLecoq921 <maxencelecoqpro@gmail.com>
2025-06-26 00:21:08 +0200

    add hash password

--- src/main/java/org/example/service/UtilisateurService.java
+++ src/main/java/org/example/service/UtilisateurService.java
@@ -2,13 +2,16 @@ package org.example.service;
 
 import org.example.dao.UtilisateurDAO;
 import org.example.model.Utilisateur;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 public class UtilisateurService {
     private final UtilisateurDAO dao = new UtilisateurDAO();
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public Utilisateur authenticate(String email, String motDePasse) {
         Utilisateur u = dao.findByEmail(email);
-        if (u != null && u.getMotDePasse().equals(motDePasse)) {
+        if (u != null && encoder.matches(motDePasse, u.getMotDePasse())) {
             return u;
         }
         return null;
@@ -18,7 +21,9 @@ public class UtilisateurService {
         if (dao.findByEmail(email) != null) {
             throw new IllegalArgumentException("Email déjà utilisé");
         }
-        Utilisateur u = new Utilisateur(email, motDePasse, role);
+        // Ici on hache le mot de passe avant de le sauvegarder
+        String hashedPassword = encoder.encode(motDePasse);
+        Utilisateur u = new Utilisateur(email, hashedPassword, role);
         return dao.save(u);
     }
 }
